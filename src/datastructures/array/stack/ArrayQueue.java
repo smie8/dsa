0f291299d00c258400093047d706e51e13ce6c27
smie8 <sami.eriksson@yahoo.fi>
2025-04-29 23:50:38 +0300

    Create array implementations of queue and stack.

---
+++ src/datastructures/array/stack/ArrayQueue.java
@@ -0,0 +1,65 @@
+package datastructures.array.stack;
+
+/**
+ * ArrayQueue is a simple implementation of a queue using an array.
+ *
+ * This queue implementation uses a wrap-around technique to handle the circular nature of the queue.
+ * Items can be inserted and removed from queue in O(1) time.
+ *
+ * This implementation could be made more efficient by removing the size variable.
+ *
+ * Note that dequeue is a double-ended queue, which means that items can be added or removed from both ends,
+ * with methods like insertLeft() and insertRight() for adding items to the left or right end of the queue.
+ * Dequeue is rarely used in practice, but it is a useful data structure to know about.
+ */
+public class ArrayQueue {
+
+    private int maxSize;
+    private int[] queue;
+    private int front;
+    private int rear;
+    private int size;
+
+    public ArrayQueue(int maxSize) {
+        this.maxSize = maxSize;
+        this.queue = new int[maxSize];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+
+    public void insert(int value) { // enqueue = put item at rear of queue
+        if (rear == maxSize - 1) {
+            rear = -1; // handle wraparound
+        }
+
+        queue[++rear] = value; // increment rear and insert value
+        size++;
+    }
+
+    public int dequeue() {
+        int temp = queue[front++]; // get value and increment front
+        if (front == maxSize) {
+            front = 0; // handle wraparound
+        }
+
+        size--;
+        return temp;
+    }
+
+    public int peek() {
+        return queue[front];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == maxSize;
+    }
+
+    public int size() {
+        return size;
+    }
+}
---
+++ src/datastructures/array/stack/ArrayStack.java
@@ -0,0 +1,43 @@
+package datastructures.array.stack;
+
+/**
+ * A stack is a data structure that follows the Last In First Out (LIFO) principle.
+ *
+ * Allows access to only one item, the last item inserted.
+ * Items can be inserted and popped in O(1) time. Does not depend on the size of the stack. No comparisons or moves are needed.
+ *
+ * Most microprocessors have a stack in their architecture. Even some old pocket calculators used stack-based architecture.
+ */
+public class ArrayStack {
+
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+
+    public ArrayStack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1;
+    }
+
+    public void push(int value) {
+        top++;
+        stackArray[top] = value;
+    }
+
+    public int pop() {
+        return stackArray[top--]; // return top item and decrement top
+    }
+
+    public int peek() {
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+}
